284be6fa09e0ce842de16d52cec2f448020e7234
Ornn8 <zzhiyuan717@gmail.com>
2026-08-24 17:01:22 +0900

    test: ratchet the checked surface to twenty-four files

--- test/typecheck-ratchet.test.mjs
+++ test/typecheck-ratchet.test.mjs
@@ -4,7 +4,7 @@ import test from 'node:test'
 
 const sourceDirectory = new URL('../src/', import.meta.url)
 const configUrl = new URL('../jsconfig.json', import.meta.url)
-const baselineCount = 23
+const baselineCount = 24
 
 async function sourceFiles(directory, prefix = 'src') {
   const entries = await readdir(directory, { withFileTypes: true })
