aa53c3bde56b01303e767baadf7c0d108493eae1
Ornn8 <zzhiyuan717@gmail.com>
2026-08-24 22:14:25 +0900

    test: ratchet pull request snapshot type coverage

--- test/typecheck-ratchet.test.mjs
+++ test/typecheck-ratchet.test.mjs
@@ -4,7 +4,7 @@ import test from 'node:test'
 
 const sourceDirectory = new URL('../src/', import.meta.url)
 const configUrl = new URL('../jsconfig.json', import.meta.url)
-const baselineCount = 25
+const baselineCount = 26
 
 async function sourceFiles(directory, prefix = 'src') {
   const entries = await readdir(directory, { withFileTypes: true })
